efc49d5ea31a75be8a6e30467cc4531814934d3c
razlaw <s6jarazl@uni-bonn.de>
2021-01-15 19:14:18 +0100

    removed printing of PACKAGE_ROOT_PATH

--- pose_graph_tracking/helpers/defaults.py
+++ pose_graph_tracking/helpers/defaults.py
@@ -4,6 +4,4 @@ file_path = abspath(__file__)
 package_name_index = file_path.rfind('pose_graph_tracking')
 PACKAGE_ROOT_PATH = file_path[:package_name_index]
 
-print("PACKAGE_ROOT_PATH", PACKAGE_ROOT_PATH)
-
 PATH_TO_DATA_DIRECTORY = PACKAGE_ROOT_PATH + 'data/'
