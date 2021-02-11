572d42bb504c0c7ad423d9c5066785e620848922
razlaw <s6jarazl@uni-bonn.de>
2021-02-11 19:58:29 +0100

    added saving of dataset description file and regeneration of dataset sample filenames using this description

--- pose_graph_tracking/data/human36m_graph_dataset_generator.py
+++ pose_graph_tracking/data/human36m_graph_dataset_generator.py
@@ -1,6 +1,8 @@
+from json import load as load_json_file, dump as save_json_file
+
 from numpy import copy, ndarray
 
-import os.path as osp
+from os.path import exists, join
 
 from pose_graph_tracking.data.normalization import PoseSequenceNormalizer
 from pose_graph_tracking.data.human36m_data_loader import Human36MDataLoader
@@ -29,12 +31,24 @@ class Human36MDataset(Dataset):
                  sample_sequence_length: int = 3):
         self.graphs_filenames = None
 
+        # Making sure path ends with a separator
+        self.data_save_directory = join(data_save_directory, "")
+
         self.path_to_data_root_directory = path_to_data_root_directory
         self.ids_of_subjects_to_load = ids_of_subjects_to_load
 
         self.sample_sequence_lenght = sample_sequence_length
 
-        super(Human36MDataset, self).__init__(data_save_directory,
+        # If there is a dataset description file in the save directory, a dataset already exists - regenerate the
+        # graphs_filenames to load the data samples
+        self.path_to_dataset_description_file = self.data_save_directory + "dataset_description.json"
+        if exists(self.path_to_dataset_description_file):
+            with open(self.path_to_dataset_description_file) as json_file:
+                self.dataset_description = load_json_file(json_file)
+                self.graphs_filenames = ['data_{}.pt'.format(i) for i in
+                                         range(self.dataset_description["number_of_samples"])]
+
+        super(Human36MDataset, self).__init__(self.data_save_directory,
                                               transform=None,
                                               pre_transform=None)
 
@@ -47,7 +61,7 @@ class Human36MDataset(Dataset):
         if self.graphs_filenames is None:
             return ["files_do_not_exist"]
         else:
-            return ['data_1.pt', 'data_2.pt', ...]
+            return self.graphs_filenames
 
     def process(self):
         i = 0
@@ -73,15 +87,21 @@ class Human36MDataset(Dataset):
                                                           ground_truth_sample,
                                                           sequence["action_id"])
 
-                torch.save(data, osp.join(self.processed_dir, 'data_{}.pt'.format(i)))
+                torch.save(data, join(self.processed_dir, 'data_{}.pt'.format(i)))
                 i += 1
+
+        dataset_description = {"number_of_samples": i,
+                               "frames_in_a_sample": self.sample_sequence_lenght,
+                               "subject_ids": self.ids_of_subjects_to_load}
+        with open(self.path_to_dataset_description_file, "w") as outfile:
+            save_json_file(dataset_description, outfile, indent=2)
 
     def len(self):
         return len(self.processed_file_names)
 
     def get(self,
             idx: int):
-        data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(idx)))
+        data = torch.load(join(self.processed_dir, 'data_{}.pt'.format(idx)))
         return data
 
     def convert_samples_to_graph_data(self,
