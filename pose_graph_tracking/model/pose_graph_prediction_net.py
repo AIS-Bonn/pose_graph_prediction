266365eee9dad8ee6d3d16380d26da0e5966def8
razlaw <s6jarazl@uni-bonn.de>
2021-03-03 16:08:35 +0100

    extracted Flatten class, generate_encoder and generate_decoder function from PoseGraphPredictionNet class into utils file

--- pose_graph_tracking/model/pose_graph_prediction_net.py
+++ pose_graph_tracking/model/pose_graph_prediction_net.py
@@ -1,38 +1,13 @@
 import torch
 
-from torch.nn import Module, Linear as Lin, ReLU, LeakyReLU, Sigmoid, LayerNorm, Conv2d, MaxPool2d
+from torch.nn import Module, ReLU, LeakyReLU, Sigmoid
 
 from torch_geometric.data import Data
 
 from pose_graph_tracking.model.pose_graph_prediction_layer import PoseGraphPredictionLayer
+from pose_graph_tracking.model.utils import generate_encoder, generate_decoder
 
 from typing import Tuple
-
-
-class Flatten(Module):
-    """
-    Copied from pytorch 1.2.0
-    Flattens a contiguous range of dims into a tensor. For use with :class:`~nn.Sequential`.
-    Args:
-        start_dim: first dim to flatten (default = 1).
-        end_dim: last dim to flatten (default = -1).
-
-    Shape:
-        - Input: :math:`(N, *dims)`
-        - Output: :math:`(N, \prod *dims)` (for the default case).
-
-    __constants__ = ['start_dim', 'end_dim']
-    start_dim: int
-    end_dim: int
-    """
-
-    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
-        super(Flatten, self).__init__()
-        self.start_dim = start_dim
-        self.end_dim = end_dim
-
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return input.flatten(self.start_dim, self.end_dim)
 
 
 # TODO: das ganze ab hier weiter an pose graph tracking anpassen - eigentlich sollte es PoseGraphPrediction heißen ..
@@ -74,13 +49,13 @@ class PoseGraphPredictionNet(Module):
         :param activation_type: Activation type used for all activation layers within the network - relu and leaky_relu.
         :param num_encoder_layers: The number of layers within the encoders.
         :param num_encoded_features_per_node: The number of features per node after being encoded.
-        :param num_node_mlp_layers: The number of layers within the node MLP of the TrackingGraphLayer.
-        :param num_hidden_units_for_node_mlp: The number of hidden units used in the node MLP of the TrackingGraphLayer.
+        :param num_node_mlp_layers: The number of layers within the node MLP of the PoseGraphPredictionLayer.
+        :param num_hidden_units_for_node_mlp: The number of hidden units used in the node MLP of the PoseGraphPredictionLayer.
         :param num_features_per_node_after_prediction: The number of features per node after the Prediction-TGL.
         :param num_features_per_node_after_association: The number of features per node after the Association-TGL.
         :param num_encoded_features_per_edge: The number of features per edge after being encoded.
-        :param num_edge_mlp_layers: The number of layers within the edge MLP of the TrackingGraphLayer.
-        :param num_hidden_units_for_edge_mlp: The number of hidden units used in the edge MLP of the TrackingGraphLayer.
+        :param num_edge_mlp_layers: The number of layers within the edge MLP of the PoseGraphPredictionLayer.
+        :param num_hidden_units_for_edge_mlp: The number of hidden units used in the edge MLP of the PoseGraphPredictionLayer.
         :param num_features_per_edge_after_prediction: The number of features per edge after the Prediction-TGL.
         :param num_features_per_edge_after_association: The number of features per edge after the Association-TGL.
         :param num_encoded_global_features: The number of global features after being encoded.
@@ -93,11 +68,10 @@ class PoseGraphPredictionNet(Module):
         assert activation_type in available_activation_types, "Requested activation type %r is not an available " \
                                                               "activation type: %r" % (activation_type,
                                                                                        available_activation_types)
-        self.activation_type = activation_type
-
-        # TODO: adapt to actual values - define values in one place
-        input_image_size_x = 80
-        input_image_size_y = 32
+        if activation_type == "relu":
+            self.activation_function = ReLU
+        else:
+            self.activation_function = LeakyReLU
 
         # Specifying number of features for edges and globals
         num_features_per_prediction_edge = 1  # distance between the two nodes this edge connects
@@ -106,18 +80,21 @@ class PoseGraphPredictionNet(Module):
         num_global_features_for_prediction = num_global_features
 
         # Encoders for node-, edge- and global_features for the prediction step
-        self.node_features_encoder = self.generateEncoder(num_features_per_node,
-                                                          num_encoded_features_per_node,
-                                                          num_encoder_layers)
-        self.node_appearance_encoder = self.generateAppearanceEncoder(input_image_size_x,
-                                                                      input_image_size_y,
-                                                                      num_encoded_features_per_node)
-        self.prediction_edge_features_encoder = self.generateEncoder(num_features_per_prediction_edge,
-                                                                     num_encoded_features_per_edge,
-                                                                     num_encoder_layers)
-        self.prediction_global_features_encoder = self.generateEncoder(num_global_features_for_prediction,
-                                                                       num_encoded_global_features,
-                                                                       num_encoder_layers)
+        self.node_features_encoder = generate_encoder(num_features_per_node,
+                                                      num_encoded_features_per_node,
+                                                      num_encoded_features_per_node,
+                                                      self.activation_function,
+                                                      num_encoder_layers)
+        self.prediction_edge_features_encoder = generate_encoder(num_features_per_prediction_edge,
+                                                                 num_encoded_features_per_edge,
+                                                                 num_encoded_features_per_edge,
+                                                                 self.activation_function,
+                                                                 num_encoder_layers)
+        self.prediction_global_features_encoder = generate_encoder(num_global_features_for_prediction,
+                                                                   num_encoded_global_features,
+                                                                   num_encoded_global_features,
+                                                                   self.activation_function,
+                                                                   num_encoder_layers)
 
         # Predicts the states of the hypotheses nodes at the next time step
         self.tracking_graph_layer_for_prediction = PoseGraphPredictionLayer(self.activation_type,
@@ -132,9 +109,11 @@ class PoseGraphPredictionNet(Module):
                                                                             num_encoded_global_features)
 
         # Encoders for edge_features for the association step
-        self.association_edge_features_encoder = self.generateEncoder(num_features_per_association_edge,
-                                                                      num_encoded_features_per_edge,
-                                                                      num_encoder_layers)
+        self.association_edge_features_encoder = generate_encoder(num_features_per_association_edge,
+                                                                  num_encoded_features_per_edge,
+                                                                  num_encoded_features_per_edge,
+                                                                  self.activation_function,
+                                                                  num_encoder_layers)
 
         # Associates corresponding detections and hypotheses and updates the hypotheses' states accordingly
         self.tracking_graph_layer_for_association = PoseGraphPredictionLayer(self.activation_type,
@@ -150,187 +129,20 @@ class PoseGraphPredictionNet(Module):
 
         # Decoders for features_of_nodes and features_of_association_edges
         # Decodes node features to their original number and meaning
-        self.node_decoder = self.generateDecoder(num_features_per_node_after_association,
-                                                 num_features_per_node,
-                                                 num_decoder_hidden_layers)
+        self.node_decoder = generate_decoder(num_features_per_node_after_association,
+                                             num_features_per_node_after_association,
+                                             num_features_per_node,
+                                             self.activation_function,
+                                             num_decoder_hidden_layers)
 
         # Decodes association edge features to represent association values between 0 and 1
-        self.edge_decoder = self.generateDecoder(num_features_per_edge_after_association,
-                                                 1,
-                                                 num_decoder_hidden_layers)
+        self.edge_decoder = generate_decoder(num_features_per_edge_after_association,
+                                             num_features_per_edge_after_association,
+                                             1,
+                                             self.activation_function,
+                                             num_decoder_hidden_layers)
         if append_sigmoid_to_association_edges_decoder:
             self.edge_decoder.add_module("decoder_sigmoid", Sigmoid())
-
-    def generateEncoder(self,
-                        number_of_inputs: int,
-                        number_of_outputs: int,
-                        number_of_layers: int = 1) -> torch.nn.Sequential:
-        """
-        Generates an encoder network wrt. input parameters.
-
-        Exemplary encoder with 3 layers:
-
-        1. layer:
-        Lin(number_of_inputs, number_of_outputs)
-
-        2. layer:
-        ReLu()
-        Lin(number_of_outputs, number_of_outputs)
-
-        3. layer:
-        ReLu()
-        Lin(number_of_outputs, number_of_outputs)
-
-        LayerNorm(number_of_outputs)
-
-        :param number_of_inputs: Number of input features/channels.
-        :param number_of_outputs: Number of output features/channels.
-        :param number_of_layers: Number of layers used for the encoder.
-        :return: Returns the generated encoder.
-        """
-        assert number_of_inputs > 0, "Number of inputs for encoder must be a positive int."
-        assert number_of_outputs > 0, "Number of outputs for encoder must be a positive int."
-        assert number_of_layers > 0, "Number of layers for encoder must be a positive int."
-
-        sequential = torch.nn.Sequential()
-        sequential.add_module("encoder_input", Lin(number_of_inputs, number_of_outputs))
-
-        if number_of_layers > 1:
-            for layer_id in range(number_of_layers - 1):
-                if self.activation_type == "relu":
-                    sequential.add_module("encoder_relu_" + str(layer_id), ReLU())
-                else:
-                    sequential.add_module("encoder_leaky_relu_" + str(layer_id), LeakyReLU())
-                sequential.add_module("encoder_hidden_" + str(layer_id), Lin(number_of_outputs, number_of_outputs))
-
-        sequential.add_module("encoder_layer_norm", LayerNorm(number_of_outputs))
-        sequential.apply(self.init_weights)
-        return sequential
-
-    def init_weights(self,
-                     m: Module):
-        negative_slope = 0 if self.activation_type == "relu" else 1e-2
-        if type(m) == Lin or type(m) == Conv2d:
-            torch.nn.init.kaiming_uniform_(m.weight, a=negative_slope, nonlinearity=self.activation_type)
-
-    def generateAppearanceEncoder(self,
-                                  input_image_size_x: int,
-                                  input_image_size_y: int,
-                                  num_encoded_features_per_node: int) -> torch.nn.Sequential:
-        """
-        Generates an encoder network for the appearances.
-
-        :return: Returns the generated encoder.
-        """
-        # TODO: Check values
-        number_of_input_channels = 3
-        kernel_size = 5
-        number_of_hidden_channels = 6
-        number_of_output_channels = 16
-        max_pool_kernel_size_x = 2
-        max_pool_kernel_size_y = 2
-
-        conv_image_size_x = int((((input_image_size_x - (kernel_size - 1)) / max_pool_kernel_size_x) - (kernel_size - 1)) / max_pool_kernel_size_x)
-        conv_image_size_y = int((((input_image_size_y - (kernel_size - 1)) / max_pool_kernel_size_y) - (kernel_size - 1)) / max_pool_kernel_size_y)
-        print("conv_image_size_x ", conv_image_size_x)
-        print("conv_image_size_y ", conv_image_size_y)
-
-        number_of_fc_0_outputs = 120
-        number_of_fc_1_outputs = 84
-
-        sequential = torch.nn.Sequential()
-        sequential.add_module("appearance_input", Conv2d(number_of_input_channels,
-                                                         number_of_hidden_channels,
-                                                         kernel_size))
-
-        if self.activation_type == "relu":
-            sequential.add_module("appearance_relu_" + str(0), ReLU())
-        else:
-            sequential.add_module("appearance_leaky_relu_" + str(0), LeakyReLU())
-        sequential.add_module("appearance_pool_" + str(0), MaxPool2d(max_pool_kernel_size_x,
-                                                                     max_pool_kernel_size_y))
-
-        sequential.add_module("appearance_hidden_" + str(0), Conv2d(number_of_hidden_channels,
-                                                                    number_of_output_channels,
-                                                                    kernel_size))
-
-        if self.activation_type == "relu":
-            sequential.add_module("appearance_relu_" + str(1), ReLU())
-        else:
-            sequential.add_module("appearance_leaky_relu_" + str(1), LeakyReLU())
-        sequential.add_module("appearance_pool_" + str(1), MaxPool2d(max_pool_kernel_size_x,
-                                                                     max_pool_kernel_size_y))
-
-        sequential.add_module("appearance_flatten", Flatten())
-
-        sequential.add_module("appearance_fc_" + str(0), Lin(number_of_output_channels *
-                                                             conv_image_size_x *
-                                                             conv_image_size_y, number_of_fc_0_outputs))
-
-        if self.activation_type == "relu":
-            sequential.add_module("appearance_relu_" + str(2), ReLU())
-        else:
-            sequential.add_module("appearance_leaky_relu_" + str(2), LeakyReLU())
-
-        sequential.add_module("appearance_fc_" + str(1), Lin(number_of_fc_0_outputs, number_of_fc_1_outputs))
-
-        if self.activation_type == "relu":
-            sequential.add_module("appearance_relu_" + str(3), ReLU())
-        else:
-            sequential.add_module("appearance_leaky_relu_" + str(3), LeakyReLU())
-
-        sequential.add_module("appearance_fc_" + str(2), Lin(number_of_fc_1_outputs, num_encoded_features_per_node))
-
-        sequential.apply(self.init_weights)
-        return sequential
-
-    def generateDecoder(self,
-                        number_of_inputs: int,
-                        number_of_outputs: int,
-                        number_of_hidden_layers: int = 1) -> torch.nn.Sequential:
-        """
-        Generates an decoder network wrt. input parameters.
-
-        Exemplary decoder with 1 hidden layer:
-
-        Input layer:
-        Lin(number_of_inputs, number_of_inputs)
-        ReLu()
-
-        Hidden layer:
-        Lin(number_of_inputs, number_of_inputs)
-        ReLu()
-
-        Output layer:
-        Lin(number_of_inputs, number_of_outputs)
-
-        :param number_of_inputs: Number of input features/channels.
-        :param number_of_outputs: Number of output features/channels.
-        :param number_of_hidden_layers: Number of hidden layers used for the decoder.
-        :return: Returns the generated decoder.
-        """
-        assert number_of_inputs > 0, "Number of inputs for decoder must be a positive int."
-        assert number_of_outputs > 0, "Number of outputs for decoder must be a positive int."
-        assert number_of_hidden_layers >= 0, "Number of hidden layers for decoder must be a positive int or zero."
-
-        sequential = torch.nn.Sequential()
-        sequential.add_module("decoder_input", Lin(number_of_inputs, number_of_inputs))
-        if self.activation_type == "relu":
-            sequential.add_module("decoder_input_relu", ReLU())
-        else:
-            sequential.add_module("decoder_input_leaky_relu", LeakyReLU())
-
-        if number_of_hidden_layers > 0:
-            for layer_id in range(number_of_hidden_layers):
-                sequential.add_module("decoder_hidden_" + str(layer_id), Lin(number_of_inputs, number_of_inputs))
-                if self.activation_type == "relu":
-                    sequential.add_module("decoder_relu_" + str(layer_id), ReLU())
-                else:
-                    sequential.add_module("decoder_leaky_relu_" + str(layer_id), LeakyReLU())
-
-        sequential.add_module("decoder_output", Lin(number_of_inputs, number_of_outputs))
-        sequential.apply(self.init_weights)
-        return sequential
 
     def forward(self,
                 data: Data) -> Tuple[torch.Tensor, torch.Tensor]:
---
+++ pose_graph_tracking/model/utils.py
@@ -0,0 +1,132 @@
+import torch
+
+from torch.nn import Module, Linear as Lin, LayerNorm, Conv2d
+
+
+class Flatten(Module):
+    """
+    Copied from pytorch 1.2.0
+    Flattens a contiguous range of dims into a tensor. For use with :class:`~nn.Sequential`.
+    Args:
+        start_dim: first dim to flatten (default = 1).
+        end_dim: last dim to flatten (default = -1).
+
+    Shape:
+        - Input: :math:`(N, *dims)`
+        - Output: :math:`(N, \prod *dims)` (for the default case).
+
+    __constants__ = ['start_dim', 'end_dim']
+    start_dim: int
+    end_dim: int
+    """
+
+    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
+        super(Flatten, self).__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return input.flatten(self.start_dim, self.end_dim)
+
+
+def init_weights(module: Module,
+                 activation_type: str = "relu"):
+    negative_slope = 0 if activation_type == "relu" else 1e-2
+    if type(module) == Lin or type(module) == Conv2d:
+        torch.nn.init.kaiming_uniform_(module.weight, a=negative_slope, nonlinearity=activation_type)
+
+
+def generate_encoder(number_of_input_channels: int,
+                     number_of_hidden_channels: int,
+                     number_of_output_channels: int,
+                     activation_function: Module,
+                     number_of_hidden_layers: int = 1) -> torch.nn.Sequential:
+    """
+    Generates an encoder network wrt. input parameters.
+
+    Exemplary encoder with 1 hidden layer and ReLu as activation function:
+
+    Input layer:
+    Lin(number_of_input_channels, number_of_hidden_channels)
+
+    Hidden layer:
+    ReLu()
+    Lin(number_of_hidden_channels, number_of_hidden_channels)
+
+    Output layer:
+    ReLu()
+    Lin(number_of_hidden_channels, number_of_output_channels)
+    LayerNorm(number_of_outputs)
+
+    :param number_of_input_channels: Number of input features/channels.
+    :param number_of_hidden_channels: Number of hidden features/channels.
+    :param number_of_output_channels: Number of output features/channels.
+    :param activation_function: Activation function.
+    :param number_of_hidden_layers: Number of hidden layers used for the encoder.
+    :return: Returns the generated encoder.
+    """
+    assert number_of_input_channels > 0, "Number of input channels for encoder must be a positive int."
+    assert number_of_hidden_channels > 0, "Number of hidden channels for encoder must be a positive int."
+    assert number_of_output_channels > 0, "Number of output channels for encoder must be a positive int."
+    assert number_of_hidden_layers >= 0, "Number of hidden layers for encoder must be a positive int or zero."
+
+    sequential = torch.nn.Sequential()
+    sequential.add_module("encoder_input_layer", Lin(number_of_input_channels, number_of_output_channels))
+
+    for layer_id in range(number_of_hidden_layers):
+        sequential.add_module("encoder_activation_function_" + str(layer_id), activation_function())
+        sequential.add_module("encoder_hidden_layer_" + str(layer_id), Lin(number_of_hidden_channels,
+                                                                           number_of_hidden_channels))
+
+    sequential.add_module("encoder_activation_function_" + str(number_of_hidden_layers), activation_function())
+    sequential.add_module("encoder_output_layer", Lin(number_of_hidden_channels, number_of_output_channels))
+    sequential.add_module("encoder_layer_norm", LayerNorm(number_of_output_channels))
+    sequential.apply(init_weights)
+    return sequential
+
+
+def generate_decoder(number_of_input_channels: int,
+                     number_of_hidden_channels: int,
+                     number_of_output_channels: int,
+                     activation_function: Module,
+                     number_of_hidden_layers: int = 1) -> torch.nn.Sequential:
+    """
+    Generates an decoder network wrt. input parameters.
+
+    Exemplary decoder with 1 hidden layer and ReLu as activation function:
+
+    Input layer:
+    Lin(number_of_input_channels, number_of_hidden_channels)
+    ReLu()
+
+    Hidden layer:
+    Lin(number_of_hidden_channels, number_of_hidden_channels)
+    ReLu()
+
+    Output layer:
+    Lin(number_of_hidden_channels, number_of_output_channels)
+
+    :param number_of_input_channels: Number of input features/channels.
+    :param number_of_hidden_channels: Number of hidden features/channels.
+    :param number_of_output_channels: Number of output features/channels.
+    :param activation_function: Activation function.
+    :param number_of_hidden_layers: Number of hidden layers used for the decoder.
+    :return: Returns the generated decoder.
+    """
+    assert number_of_input_channels > 0, "Number of input channels for decoder must be a positive int."
+    assert number_of_hidden_channels > 0, "Number of hidden channels for decoder must be a positive int."
+    assert number_of_output_channels > 0, "Number of output channels for decoder must be a positive int."
+    assert number_of_hidden_layers >= 0, "Number of hidden layers for decoder must be a positive int or zero."
+
+    sequential = torch.nn.Sequential()
+    sequential.add_module("decoder_input", Lin(number_of_input_channels, number_of_hidden_channels))
+    sequential.add_module("decoder_input_activation_function", activation_function())
+
+    for layer_id in range(number_of_hidden_layers):
+        sequential.add_module("decoder_hidden_" + str(layer_id), Lin(number_of_hidden_channels,
+                                                                     number_of_hidden_channels))
+        sequential.add_module("decoder_activation_function_" + str(layer_id), activation_function())
+
+    sequential.add_module("decoder_output", Lin(number_of_hidden_channels, number_of_output_channels))
+    sequential.apply(init_weights)
+    return sequential
