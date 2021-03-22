d6167dd89ec3d4893c5d71b7912806b3e7142c28
razlaw <s6jarazl@uni-bonn.de>
2021-03-22 17:15:24 +0100

    added functionality to visualize the next frame while in pause mode by pressing the right arrow key

--- pose_graph_tracking/visualization/stoppable_sequential_visualizer.py
+++ pose_graph_tracking/visualization/stoppable_sequential_visualizer.py
@@ -4,6 +4,7 @@ from matplotlib.backend_bases import Event
 from mpl_toolkits.mplot3d import Axes3D
 
 from time import sleep
+
 from typing import Any
 
 
@@ -11,6 +12,7 @@ class StoppableSequentialVisualizer(object):
     """
     Abstract class providing the functionality to visualize a sequence of data frame by frame.
     The visualization can be stopped and continued by pressing the space bar.
+    While stopped, the next frame can be visualized by pressing the right arrow key.
 
     A subclass has to implement the _draw_plot method, providing data to the plot.
     Calling the update_plot method updates the visualization.
@@ -18,6 +20,7 @@ class StoppableSequentialVisualizer(object):
     def __init__(self,
                  window_title: str = "Sequential Visualization"):
         self.is_requested_to_pause = False
+        self.is_next_frame_requested = False
 
         # Activate interactive mode for plots
         ion()
@@ -26,6 +29,7 @@ class StoppableSequentialVisualizer(object):
         self.fig = figure()
         self.fig.canvas.set_window_title(window_title)
         self.fig.canvas.mpl_connect('key_press_event', self._request_to_pause_or_continue_visualization)
+        self.fig.canvas.mpl_connect('key_press_event', self._request_next_frame)
         # Updates the visualization, without forcing the visualization window into the foreground
         self._update_visualization = self.fig.canvas.flush_events
 
@@ -34,13 +38,20 @@ class StoppableSequentialVisualizer(object):
 
     def _clear_plot(self):
         self.plot3d.cla()
-        self.plot3d.text2D(0.05, 0.95, "Press key to pause/continue", transform=self.plot3d.transAxes)
+        self.plot3d.text2D(0.05,
+                           0.95,
+                           "Press space bar to pause/continue - right arrow key to show next frame",
+                           transform=self.plot3d.transAxes)
 
     def _request_to_pause_or_continue_visualization(self,
                                                     event: Event):
         if event.key == " ":
             self.is_requested_to_pause = not self.is_requested_to_pause
 
+    def _request_next_frame(self,
+                            event: Event):
+        if event.key == "right" and self.is_requested_to_pause:
+            self.is_next_frame_requested = True
 
     def update_plot(self,
                     data: Any):
@@ -54,6 +65,10 @@ class StoppableSequentialVisualizer(object):
         while self.is_requested_to_pause:
             sleep(0.01)  # 100 frames per second
             self._update_visualization()
+            # Break out of pause if next frame is requested, but end up in pause again after next frame is visualized
+            if self.is_next_frame_requested:
+                self.is_next_frame_requested = False
+                break
 
     def _draw_plot(self,
                    data: Any):
