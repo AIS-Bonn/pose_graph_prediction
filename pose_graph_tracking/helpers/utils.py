9076728a7b7964013959483a1a42f7ec66e695f2
razlaw <s6jarazl@uni-bonn.de>
2021-03-24 18:27:52 +0100

    added function to load the trained model weights into a model

--- pose_graph_tracking/helpers/utils.py
+++ pose_graph_tracking/helpers/utils.py
@@ -45,3 +45,40 @@ def load_config_file(path_to_config_file: str) -> dict:
     else:
         print("Config file does not exist. Exiting. Provided file path is ", path_to_config_file)
         exit(-1)
+
+
+def load_model_weights(model: Any,
+                       path: str):
+    """
+    Checks whether the network was trained in parallel on several GPUs or not and loads the parameters accordingly.
+    This is a workaround due to training with DataParallel and visualizing the training results without it.
+    DataParallel adds a "module." prefix to every layer name in the dict.
+    To use the network on a CPU or GPU without DataParallel this prefix has to be removed.
+
+    :param model: The model of the network.
+    :param path: Path to the trained and stored model parameters.
+    :return: Model with trained parameters
+    """
+    # Load stored network parameters that were trained using DataParallel
+    state_dict = torch.load(path, map_location=lambda storage, loc: storage)
+    # Create new OrderedDict that does not contain `module.`
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    model_was_probably_trained_with_data_parallel = False
+    for k, v in state_dict.items():
+        if str(k).startswith("module."):
+            # Remove `module.` prefix
+            name = k[7:]
+            new_state_dict[name] = v
+            model_was_probably_trained_with_data_parallel = True
+        else:
+            new_state_dict = state_dict
+            model_was_probably_trained_with_data_parallel = False
+            break
+
+    if model_was_probably_trained_with_data_parallel:
+        print("All names of the model started with \"module.\". It's assumed the model was trained using DataParallel.")
+    else:
+        print("Model doesn't seem to be trained with DataParallel. Loading parameters the usual way.")
+
+    model.load_state_dict(new_state_dict)
