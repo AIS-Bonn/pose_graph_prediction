fb4a150b3564d9a570e812439658cb293388f737
razlaw <s6jarazl@uni-bonn.de>
2021-03-25 20:26:27 +0100

    training uses augmented data generator

--- pose_graph_tracking/training/train_model.py
+++ pose_graph_tracking/training/train_model.py
@@ -1,4 +1,4 @@
-from pose_graph_tracking.data.human36m_graph_dataset_generator import Human36MDataset
+from pose_graph_tracking.data.augmenting_human36m_graph_in_memory_dataset_generator import AugmentingHuman36MDataset
 
 from pose_graph_tracking.helpers.defaults import PATH_TO_DATA_DIRECTORY, PATH_TO_CONFIG_DIRECTORY
 from pose_graph_tracking.helpers.utils import get_model, make_deterministic_as_possible, load_config_file
@@ -11,11 +11,11 @@ training_hyperparameter_config = load_config_file(path_to_training_hyperparamete
 
 make_deterministic_as_possible()
 
-training_data = Human36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human36M/training_data",
-                                ids_of_subjects_to_load=[1, 6, 7, 8, 9, 11])
+training_data = AugmentingHuman36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human36M/augmenting_training_data",
+                                          ids_of_subjects_to_load=[1, 6, 7, 8, 9, 11])
 
-test_data = Human36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human36M/test_data",
-                            ids_of_subjects_to_load=[5])
+test_data = AugmentingHuman36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human36M/augmenting_test_data",
+                                      ids_of_subjects_to_load=[5])
 
 
 path_to_model_config_file = PATH_TO_CONFIG_DIRECTORY + "model_config.json"
