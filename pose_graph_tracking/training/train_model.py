8b1aa88b763e06249f2cb5435dcb99bce33f526e
razlaw <s6jarazl@uni-bonn.de>
2021-03-26 16:33:53 +0100

    added option to continue a trained network

--- pose_graph_tracking/training/train_model.py
+++ pose_graph_tracking/training/train_model.py
@@ -1,8 +1,8 @@
 from pose_graph_tracking.data.augmenting_human36m_graph_in_memory_dataset_generator import \
     AugmentingHuman36MDataset, Human36MDataset
 
-from pose_graph_tracking.helpers.defaults import PATH_TO_DATA_DIRECTORY, PATH_TO_CONFIG_DIRECTORY
-from pose_graph_tracking.helpers.utils import get_model, make_deterministic_as_possible, load_config_file
+from pose_graph_tracking.helpers.defaults import PATH_TO_DATA_DIRECTORY, PATH_TO_CONFIG_DIRECTORY, PATH_TO_MODEL
+from pose_graph_tracking.helpers.utils import get_model, load_model_weights, make_deterministic_as_possible, load_config_file
 
 from pose_graph_tracking.training.trainer import Trainer
 
@@ -22,6 +22,9 @@ test_data = Human36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human3
 path_to_model_config_file = PATH_TO_CONFIG_DIRECTORY + "model_config.json"
 model_config = load_config_file(path_to_model_config_file)
 model = get_model(model_config)
+if training_hyperparameter_config["continue_learning_from_existing_model"]:
+    print("Continuing training from existing model in ", PATH_TO_MODEL)
+    load_model_weights(model, PATH_TO_MODEL)
 
 trainer = Trainer(model,
                   training_data,
