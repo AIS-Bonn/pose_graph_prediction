f1c1e3fc6db16b869e7a3e77bd9ccdd0be48950a
Razlaw <jan.razlaw@gmx.de>
2021-10-29 12:02:31 +0200

    Use best model on test data for visualization

--- pose_graph_tracking/visualization/human36m_sequential_prediction_visualizer.py
+++ pose_graph_tracking/visualization/human36m_sequential_prediction_visualizer.py
@@ -1,6 +1,6 @@
 from pose_graph_tracking.data.human36m_graph_in_memory_dataset_generator import Human36MDataset
 
-from pose_graph_tracking.helpers.defaults import PATH_TO_CONFIG_DIRECTORY, PATH_TO_DATA_DIRECTORY, PATH_TO_MODEL
+from pose_graph_tracking.helpers.defaults import PATH_TO_BEST_TEST_MODEL, PATH_TO_CONFIG_DIRECTORY, PATH_TO_DATA_DIRECTORY
 from pose_graph_tracking.helpers.utils import get_model, load_model_weights, load_config_file, \
     make_deterministic_as_possible
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     path_to_model_config_file = PATH_TO_CONFIG_DIRECTORY + "model_config.json"
     model_config = load_config_file(path_to_model_config_file)
     model = get_model(model_config)
-    load_model_weights(model, PATH_TO_MODEL)
+    load_model_weights(model, PATH_TO_BEST_TEST_MODEL)
 
     visualization_data = Human36MDataset(data_save_directory=PATH_TO_DATA_DIRECTORY + "Human36M/visualization_data",
                                          ids_of_subjects_to_load=[11])
